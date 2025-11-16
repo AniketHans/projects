ec5cb8fdcde66c898bbb4c04ba22121c138752f0
Aniket Hans <aniketdharya@gmail.com>
2025-11-17 00:40:09 +0530

    Added dockerfile and ngnix.conf

--- RealTime-Code-Editor/Frontend/src/App.jsx
+++ RealTime-Code-Editor/Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
-import "./app.css";
+import "./App.css";
 
 function App() {
   return (
